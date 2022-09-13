5663a49353da6c95463230e493670040a44c9205
Simran Kalirai <kaliraisimran@gmail.com>
2022-09-13 22:15:38 +0000

    when this commit is merged, it will dynamically change the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
   console.log("ready");
-  const texting = $( "#tweet-text" );
-  console.log(texting);
-$( texting ).on("input", function () {
-  console.log(this.value);
+  const texting = $( "#tweet-text" )[0];
+  const maxChars = 140;
   
-});
+
+  $(texting).on("input", function () {
+    const counter = ($(this).parents("form").find(".counter")[0]);
+    let isNegative = counter.classList.contains("negative");
+    let charsRemaining = maxChars - Number(this.value.length);
+    if (charsRemaining < 0) {
+      $( counter ).addClass( "negative" );
+    }
+    if (charsRemaining >= 0 && isNegative) {
+      $( counter ).removeClass( "negative" );
+    }
+    $(counter).val(charsRemaining);
+
+  });
 
 });
 
